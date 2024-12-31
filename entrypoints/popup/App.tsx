a62a24d541951fb03036ce19a2d1b36460d172d7
Eduardo Olivares <28717476+eduardo-olivares1@users.noreply.github.com>
2024-12-30 18:18:05 -0800

    fmt

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,27 +1,27 @@
-import { useState } from 'react';
-import reactLogo from '@/assets/react.svg';
-import wxtLogo from '/wxt.svg';
+import { useState } from "react";
+import reactLogo from "@/assets/react.svg";
+import wxtLogo from "/wxt.svg";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-    <a />
+      <a />
       <div className="max-w-[1280px] mx-auto p-8 text-center">
-        <div className='flex justify-center items-center'>
+        <div className="flex justify-center items-center">
           <a href="https://wxt.dev" target="_blank">
-            <img 
-              src={wxtLogo} 
-              className="h-24 p-6 transition-filter duration-300 hover:drop-shadow-[0_0_0.5em_#54bc4ae0]" 
-              alt="WXT logo" 
+            <img
+              src={wxtLogo}
+              className="h-24 p-6 transition-filter duration-300 hover:drop-shadow-[0_0_0.5em_#54bc4ae0]"
+              alt="WXT logo"
             />
           </a>
           <a href="https://react.dev" target="_blank">
-            <img 
-              src={reactLogo} 
-              className="h-24 p-6 transition-filter duration-300 hover:drop-shadow-[0_0_0.5em_#61dafbaa] animate-[spin_20s_linear_infinite]" 
-              alt="React logo" 
+            <img
+              src={reactLogo}
+              className="h-24 p-6 transition-filter duration-300 hover:drop-shadow-[0_0_0.5em_#61dafbaa] animate-[spin_20s_linear_infinite]"
+              alt="React logo"
             />
           </a>
         </div>
